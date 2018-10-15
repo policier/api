847b81e62b420b8fe640d9db8817624e5ee5d980
owen mougoue <owen.mougoue@cgi.com>
2018-10-15 12:04:58 +0200

    add helm delete all options

--- jenkins/jenkinsUtils.groovy
+++ jenkins/jenkinsUtils.groovy
@@ -11,6 +11,15 @@ def build(String currentContext){
                 
                 cd discountservice && mvn install docker:build && cd ..
             """
+
+        bat """
+                SET DOCKER_TLS_VERIFY=1
+                SET DOCKER_HOST=tcp://192.168.99.100:2376
+                SET DOCKER_CERT_PATH=C:\\Users\\mougoueo\\.minikube\\certs
+                SET DOCKER_API_VERSION=1.35
+                
+                cd discountservice && mvn install docker:build && cd ..
+            """
     }
 
 }
