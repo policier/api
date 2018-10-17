466dfdfa561fefc7353f7f8355cbdb4770fb1dd9
owen mougoue <owen.mougoue@cgi.com>
2018-10-17 16:53:14 +0200

    remove the port from the url

--- jenkins/jenkinsUtils.groovy
+++ jenkins/jenkinsUtils.groovy
@@ -13,10 +13,7 @@ def build(String currentContext){
             """
 
         bat """
-                SET DOCKER_TLS_VERIFY=1
-                SET DOCKER_HOST=tcp://192.168.99.100:2376
-                SET DOCKER_CERT_PATH=C:\\Users\\mougoueo\\.minikube\\certs
-                SET DOCKER_API_VERSION=1.35
+               
                 
                 cd discountservice && mvn install docker:build && cd ..
             """
